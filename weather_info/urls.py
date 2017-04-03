7ed02ac1f5a772be7770acc30fc0849768fb5f42
namanksharma <sharmanaman09@gmail.com>
2017-04-03 18:38:49 +0530

    Update urls.py

--- weather_info/urls.py
+++ weather_info/urls.py
@@ -12,13 +12,22 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from weather_info import views
+from weather_info import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = patterns('',
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',views.weather_info, name='weather_info'),
     url(r'^save_city_data/$',views.save_city_data, name='save_city_data'),
     url(r'^get_city_data/$',views.get_city_data, name='get_city_data'),
-]
+
+)
+# + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+
+urlpatterns += patterns('',
+             (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.BASE_DIR+'/weather_info/static', 'show_indexes':True}),
+         )
